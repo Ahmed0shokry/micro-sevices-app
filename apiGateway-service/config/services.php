9afc47054172167ceed996477219fb9d0754e70f
ahmed0shokry <ahmed.dev.ahmed@gmail.com>
2020-05-06 17:38:31 +0200

    update config services.php to accept secret keys

--- apiGateway-service/config/services.php
+++ apiGateway-service/config/services.php
@@ -3,8 +3,11 @@
 return [
     'authors' => [
         'base_url' => env('AUTHOR_SERVICE_URL'),
+        'secret' => env('AUTHOR_SERVICE_SECRET'),
+
     ],
     'books' => [
         'base_url' => env('BOOK_SERVICE_URL'),
+        'secret' => env('BOOK_SERVICE_SECRET'),
     ],
 ];
